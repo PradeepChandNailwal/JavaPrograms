a99acc6ea2882d70aa11c365179fffc869465962
PradeepChandNailwal <50335845+PradeepChandNailwal@users.noreply.github.com>
2023-05-16 23:41:12 +0530

    Add files via upload

--- EquilateralTriangleProgram.java
+++ EquilateralTriangleProgram.java
@@ -13,15 +13,14 @@ public class EquilateralTriangleProgram {
 
 		for (i = 1; i <= n; i++) {
 
-		for (j = i; j <= n; j++) {
-			System.out.print(" ");
-		}
-		for (j = 1; j <= i; j++) {
-			System.out.print("* ");
-		}
+			for (j = i; j <= n; j++) {
+				System.out.print(" ");
+			}
+			for (j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
 			System.out.println("");
 		}
 	}
-
 
 }
