993a2b85ed7ee7e2d318c28b16fef247f08f2305
PradeepChandNailwal <50335845+PradeepChandNailwal@users.noreply.github.com>
2023-05-23 20:20:13 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,16 @@
+package com.org.main;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+		int k;
+		k = i;
+		i = j;
+		j = k;
+		System.out.println("i=" + i);
+		System.out.println("j=" + j);
+	}
+
+}
